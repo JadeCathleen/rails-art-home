df485209787c5857d203c9c5a70bb2360a11ea3c
Jade Cormier <cormier.jade@gmail.com>
2021-11-15 17:21:30 +0100

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artpieces do
-    resources :rentals only: [:new, :create]
+    resources :rentals, only: [:new, :create]
+    resources :wishes, only: [:new, :create]
   end
+
+  resources :wishes, only: [:index, :destroy]
+
+  resources :rentals, only: [:index, :edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
 end
