dac67e6a3c5ca1aed5fd5832ecdd60c6a7e82dfa
Jade Cormier <cormier.jade@gmail.com>
2021-11-16 16:55:32 +0100

    rentals controller + views ok

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,4 +3,5 @@ class Rental < ApplicationRecord
   belongs_to :user
 
   validates :start_time, :end_time, presence: true
+  validates :status, inclusion: { in: ['pending', 'accepted', 'rejected', 'cancelled'] }
 end
---
+++ app/policies/rental_policy.rb
@@ -0,0 +1,24 @@
+class RentalPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    record.artpiece.user != user
+  end
+
+  def change_status?
+    record.artpiece.user == user
+  end
+
+  def update?
+    record.artpiece.user != user
+  end
+
+end
