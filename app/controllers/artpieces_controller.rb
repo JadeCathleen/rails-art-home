9f66d2f5603798c6d6a7fb16d08c2cc887f0ca5e
damien jouaneau <damienjouaneau@gmail.com>
2021-11-16 11:38:05 +0100

    pundit controller manque view

--- app/controllers/artpieces_controller.rb
+++ app/controllers/artpieces_controller.rb
@@ -1,18 +1,24 @@
 class ArtpiecesController < ApplicationController
   before_action :set_artpiece, only: [:show, :edit, :update, :destroy]
 
+
   def index
+    authorize @artpiece
+    @artpieces = policy_scope(Artpiece).order(created_at: :desc)
     @artpieces = Artpiece.all
   end
 
   def show
+    authorize @artpiece
   end
 
   def new
+    authorize @artpiece
     @artpiece = Artpiece.new
   end
 
   def create
+    authorize @artpiece
     @artpiece = Artpiece.new(artpiece_params)
     @artpiece.user = User.find(params[:user_id])
     if @artpiece.save
@@ -23,14 +29,17 @@ class ArtpiecesController < ApplicationController
   end
 
   def edit
+    authorize @artpiece
   end
 
   def update
+    authorize @artpiece
     @artpiece.update(artpiece_params)
     redirect_to artpieces_path(@artpiece)
   end
 
   def destroy
+    authorize @artpiece
     @artpiece.destroy
     redirect_to artpieces_path
   end
