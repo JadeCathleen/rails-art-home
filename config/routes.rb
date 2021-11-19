de03fa22bee76980ca606c82f221f0ff77c9fe9e
Jade Cormier <cormier.jade@gmail.com>
2021-11-19 12:47:01 +0100

    rentals forms ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artpieces do
-    resources :rentals, only: [:new, :create]
+    resources :rentals, only: [:new, :create, :edit, :update]
     resources :wishes, only: [:new, :create]
   end
 
   resources :wishes, only: [:index, :destroy]
 
-  resources :rentals, only: [:index, :edit, :update] do
+  resources :rentals, only: [:index] do
     member do
       patch 'change_status'
     end
